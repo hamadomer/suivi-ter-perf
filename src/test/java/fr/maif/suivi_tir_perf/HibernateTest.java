27642c5cff36b2cf7d05b25c0916fa22dc59271d
HAMED ELBASHEIR MOHAMEDELNOUR Omer <omer.hamad.elbashier@maif.fr>
2024-07-30 15:10:50 +0200

    jpa insted of hibernate

--- src/test/java/fr/maif/suivi_tir_perf/HibernateTest.java
+++ src/test/java/fr/maif/suivi_tir_perf/HibernateTest.java
@@ -1,11 +1,9 @@
 package fr.maif.suivi_tir_perf;
 
 import fr.maif.suivi_tir_perf.models.Fonction;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,52 +13,40 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class HibernateTest {
 
-    private SessionFactory sessionFactory;
+    private EntityManagerFactory entityManagerFactory;
 
     @BeforeEach
     protected void setUp() throws Exception {
-        // A SessionFactory is set up once for an application!
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
-                .build();
-        try {
-            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-        }
-        catch (Exception e) {
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
-            StandardServiceRegistryBuilder.destroy( registry );
-        }
+        entityManagerFactory = Persistence.createEntityManagerFactory("tirperf");
     }
 
     @AfterEach
     protected void tearDown() throws Exception {
-        if ( sessionFactory != null ) {
-            sessionFactory.close();
+        if (entityManagerFactory != null) {
+            entityManagerFactory.close();
         }
     }
 
     @Test
     public void testCreateFonction() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
         Fonction fonction = new Fonction();
         fonction.setName("Test Fonction");
 
-        session.persist(fonction);
-        session.getTransaction().commit();
-        session.close();
+        entityManager.persist(fonction);
+        entityManager.getTransaction().commit();
+        entityManager.close();
 
-        session = sessionFactory.openSession();
-        session.beginTransaction();
-        Fonction persistedFonction = session.get(Fonction.class, fonction.getId()); //TODO do .persist or .commit update the object instant and set the id !!
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        Fonction persistedFonction = entityManager.find(Fonction.class, fonction.getId());
 
         assertNotNull(persistedFonction);
         assertEquals(fonction.getName(), persistedFonction.getName());
 
-        session.getTransaction().commit();
-        session.close();
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
-
 }
